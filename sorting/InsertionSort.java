5a2783e10d1c9e2c48770b054c8f7233226a904c
CHANDRAMOHANnegi <cmsingh1721997@gmail.com>
2020-11-29 14:46:07 +0530

    Insertion sort

--- sorting/InsertionSort.java
+++ sorting/InsertionSort.java
@@ -1,22 +1,17 @@
 
 class InsertionSort {
 
-    public static void swap(int[] nums, int i, int n) {
-        int temp = nums[n];
-        nums[n] = nums[i];
-        nums[i] = temp;
-    }
-
     public static void sort(int nums[]) {
         for (int i = 1; i < nums.length - 1; i++) {
             int value = nums[i];
             int j = i;
-            while (value > nums[j]) {
+            while (j > 0 && value < nums[j - 1]) {
+                nums[j] = nums[j - 1];
                 j--;
-                swap(nums, j, j + 1);
             }
+            nums[j] = value;
         }
-    } 
+    }
 
     public static void main(String[] args) {
         int nums[] = new int[] { 1, 3, 4, 3, 5 };
