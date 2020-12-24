a2a167bd80055a5acca17c8f86501652b92fe423
CHANDRAMOHANnegi <cmsingh1721997@gmail.com>
2020-12-25 01:09:00 +0530

    swap pair done

--- linkedList/SwapNodesInPairs.java
+++ linkedList/SwapNodesInPairs.java
@@ -1,31 +1,33 @@
-public class SwapNodesInPairs extends LinkedList {
-    public static LinkedList swapPairs(Node head) {
-        LinkedList finalList = new LinkedList();
-        if (head.next == null) {
-            finalList.head = head;
-            return finalList;
+/**
+ *
+ * 
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+
+        if (head == null || head.next == null) {
+            return head;
         }
-        Node pointer = new Node(0);
+
+        ListNode pointer = new ListNode(0);
         pointer.next = head;
-        finalList.head = pointer.next.next;
+        head = pointer.next.next;
 
         while (pointer.next != null) {
-            Node first = pointer.next;
+            ListNode first = pointer.next;
 
             if (first.next == null) {
-                return finalList;
+                return head;
             }
-            Node second = first.next;
-            System.out.println(pointer.data + " " + first.data + " " + second.data);
-            Node temp = second.next;
+            ListNode second = first.next;
+            ListNode temp = second.next;
 
             second.next = first;
-
             first.next = temp;
+
             pointer.next = second;
             pointer = first;
-            System.out.println(first.data + " " + second.data);
         }
-        return finalList;
+        return head;
     }
 }
