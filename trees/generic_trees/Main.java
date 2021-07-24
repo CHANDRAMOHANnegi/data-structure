3b2d29acd8bc99354d609c074bb565b790cdc6b4
CHANDRAMOHANnegi <cmsingh1721997@gmail.com>
2021-07-24 15:57:10 +0530

    leveorder traversal ,zigzag ; mirror ,removeleaf , linearize ;  find , nodetoroot path

--- trees/generic_trees/Main.java
+++ trees/generic_trees/Main.java
@@ -2,10 +2,21 @@ package generic_trees;
 
 import java.util.*;
 
+import jdk.nashorn.internal.objects.annotations.Where;
+
 public class Main {
 	private static class Node {
 		int data;
 		ArrayList<Node> children = new ArrayList<>();
+
+		Node() {
+
+		}
+
+		Node(int data) {
+			this.data = data;
+		}
+
 	}
 
 	public static void display(Node node) {
@@ -82,6 +93,219 @@ public class Main {
 		System.out.println("node post" + node.data);
 	}
 
+	public static void levelordertraversal(Node node) {
+
+		Queue<Node> que = new ArrayDeque<>();
+		que.add(node);
+
+		while (que.size() > 0) {
+			node = que.remove();
+			System.out.println(node.data + "  ");
+			for (Node child : node.children) {
+				// System.out.println(child.data + " ");
+				que.add(child);
+			}
+		}
+		System.out.println(".");
+	}
+
+	public static void levelorderlinewisetraversal(Node node) {
+		Queue<Node> mq = new ArrayDeque<>();
+		mq.add(node);
+
+		Queue<Node> cq = new ArrayDeque<>();
+		while (mq.size() > 0) {
+			node = mq.remove();
+			System.out.print(node.data + "  ");
+			for (Node child : node.children) {
+				cq.add(child);
+				// System.out.println(child.data + " ");
+			}
+			if (mq.size() == 0) {
+				mq = cq;
+				cq = new ArrayDeque<>();
+				System.out.println();
+			}
+		}
+	}
+
+	public static void levelorderlinewiseWithNulltraversal(Node node) {
+		ArrayDeque<Node> mq = new ArrayDeque<>();
+		mq.add(node);
+		mq.add(new Node(-1));
+
+		while (mq.size() > 0) {
+			node = mq.remove();
+
+			if (node.data == -1) {
+				if (mq.size() > 0) {
+					mq.add(new Node(-1));
+					System.out.println();
+				}
+			} else {
+
+				System.out.print(node.data + "  ");
+
+				for (Node child : node.children) {
+					mq.add(child);
+				}
+
+			}
+		}
+	}
+
+	public static void levelorderzigzagtraversal(Node node) {
+		Stack<Node> ms = new Stack<>();
+		ms.push(node);
+
+		int level = 0;
+		Stack<Node> cs = new Stack<>();
+		while (ms.size() > 0) {
+			node = ms.pop();
+			System.out.print(node.data + "  ");
+
+			if (level % 2 == 1) {
+				for (int i = 0; i < node.children.size(); i++) {
+					Node child = node.children.get(i);
+					cs.push(child);
+				}
+			} else {
+				for (int i = node.children.size() - 1; i >= 0; i--) {
+					Node child = node.children.get(i);
+					cs.push(child);
+				}
+			}
+
+			if (ms.size() == 0) {
+				ms = cs;
+				cs = new Stack<>();
+				level++;
+				System.out.println();
+			}
+		}
+	}
+
+	public static void levelOrderzigziagAlternateTraversa(Node node) {
+
+	}
+
+	public static void levelOrderzigziagAlternateTraversal(Node node) {
+		Stack<Node> ms = new Stack<>();
+		ms.push(node);
+
+		int level = 0;
+		Stack<Node> cs = new Stack<>();
+
+		while (ms.size() > 0) {
+			node = ms.pop();
+			System.out.print(node.data + " ");
+
+			if (level % 2 == 1) {
+				for (int i = 0; i < node.children.size(); i++) {
+					Node child = node.children.get(i);
+					cs.push(child);
+				}
+			} else {
+				for (int i = node.children.size() - 1; i >= 0; i--) {
+					Node child = node.children.get(i);
+					cs.push(child);
+				}
+			}
+
+			if (ms.size() == 0) {
+				ms = cs;
+				cs = new Stack<>();
+				level++;
+				System.out.println();
+			}
+		}
+	}
+
+	/**
+	 * MIRROR
+	 */
+
+	public static void mirror(Node node) {
+		for (Node child : node.children) {
+			mirror(child);
+		}
+		Collections.reverse(node.children);
+	}
+
+	/**
+	 * REMOVE LEAFS
+	 * 
+	 * @param args
+	 **/
+
+	public static void removeLeafs(Node node) {
+		for (int i = node.children.size() - 1; i >= 0; i--) {
+			Node child = node.children.get(i);
+			if (child.children.size() == 0) {
+				node.children.remove(child);
+			}
+		}
+
+		for (Node child : node.children) {
+			removeLeafs(child);
+		}
+	}
+
+	/**
+	 * LINEARIZE TREE *
+	 **/
+
+	public static void linearize(Node node) {
+		for (Node child : node.children) {
+			linearize(child);
+		}
+
+		while (node.children.size() > 1) {
+			Node lastchild = node.children.remove(node.children.size() - 1);
+			Node secondlastchild = node.children.get(node.children.size() - 1);
+
+			Node stl = getTail(secondlastchild);
+			stl.children.add(lastchild);
+		}
+	}
+
+	public static Node getTail(Node node) {
+		while (node.children.size() == 1) {
+			node = node.children.get(0);
+		}
+		return node;
+	}
+
+	public static ArrayList nodetoroot(Node node, int data) {
+		if (node.data == data) {
+			ArrayList<Integer> list = new ArrayList<>();
+			list.add(node.data);
+			return list;
+		}
+
+		for (Node child : node.children) {
+			ArrayList list = nodetoroot(child, data);
+			if (list.size() > 0) {
+				list.add(node.data);
+				return list;
+			}
+		}
+		return new ArrayList<>();
+	}
+
+	public static Boolean find(Node node, int num) {
+		if (node.data == num) {
+			return true;
+		}
+		for (Node child : node.children) {
+			Boolean found = find(child, num);
+			if (found) {
+				return true;
+			}
+		}
+		return false;
+	}
+
 	public static void main(String[] args) {
 		int[] arr = { 10, 20, 50, -1, 60, -1, -1, 30, 70, -1, 80, 110, -1, 120, -1, -1, 90, -1, -1, 40, 100, -1, -1,
 				-1 };
@@ -107,7 +331,19 @@ public class Main {
 		// System.out.println(max(root));
 		// System.out.println(height(root, 0));
 		// System.out.println(height2(root));
-		traversal(root);
+		// traversal(root);
+		// levelordertraversal(root);
+		// levelorderlinewisetraversal(root);
+		// levelorderzigzagtraversal(root);
+		// levelorderlinewiseWithNulltraversal(root);
+		// levelOrderzigziagAlternateTraversal(root);
+		// mirror(root);
+		// removeLeafs(root);
+		// linearize(root);
+		// display(root);
+		// levelorderzigzagtraversal(root);
+		// System.out.println(find(root, 44));
+		System.out.println(nodetoroot(root, 100));
 	}
 
 }
